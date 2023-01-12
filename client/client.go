db894a91393485df5faaa178e945847b9154bd7a
Steven <steve.rutherford12@gmail.com>
2023-01-13 11:01:06 +1300

    Get User

--- client/client.go
+++ client/client.go
@@ -1,6 +1,11 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 type Client struct {
 	Api_key string
@@ -17,4 +22,46 @@ func New(api_key string, api_url string, timeout int, version int, session strin
 
 func (c Client) PrintConfig() {
 	fmt.Println(c.Api_url)
+}
+
+func (c Client) defaultHeaders() http.Header {
+	return http.Header{
+		"Accept":       {"*/*"},
+		"Content-Type": {"application/json"},
+		"User-Agent":   {c.userAgent()},
+	}
+}
+
+func (c Client) userAgent() string {
+	return "Authsignal Python v1" // todo make module version dynamic
+}
+
+func (c Client) GetUser(user_id string) string {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", c.Api_url+"/v1/users/"+user_id, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header = c.defaultHeaders()
+	req.SetBasicAuth(c.Api_key, "")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sb := string(body)
+	return sb
 }
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"authsignalgo/client"
+	"fmt"
 )
 
 func main() {
 	c := client.Client{Api_url: "https://dev-signal.authsignal.com", Api_key: "mURA8eRlODYiEd+butwzGp/5GTaC3tyEoFcufvVxtd0OoJYVK9EVuA==", Version: 1, Timeout: 2, Session: ""}
 	c.PrintConfig()
+	fmt.Println(c.GetUser("1"))
 }
