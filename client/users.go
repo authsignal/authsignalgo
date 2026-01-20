4de2e088cb6de7e567db55194de5f50db35c5554
Ashutosh Bhadauriya <ashutoshb455@gmail.com>
2026-01-20 08:19:03 +0530

    add queryusers endpoint (#14)

--- client/users.go
+++ client/users.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/url"
+	"strconv"
 )
 
 func (c Client) GetUser(input GetUserRequest) (GetUserResponse, error) {
@@ -51,4 +53,45 @@ func (c Client) DeleteUser(input DeleteUserRequest) error {
 	}
 
 	return nil
+}
+
+func (c Client) QueryUsers(input QueryUsersRequest) (QueryUsersResponse, error) {
+	params := url.Values{}
+
+	if input.Username != "" {
+		params.Set("username", input.Username)
+	}
+	if input.Email != "" {
+		params.Set("email", input.Email)
+	}
+	if input.PhoneNumber != "" {
+		params.Set("phoneNumber", input.PhoneNumber)
+	}
+	if input.Token != "" {
+		params.Set("token", input.Token)
+	}
+	if input.Limit != nil {
+		params.Set("limit", strconv.Itoa(*input.Limit))
+	}
+	if input.LastEvaluatedUserId != "" {
+		params.Set("lastEvaluatedUserId", input.LastEvaluatedUserId)
+	}
+
+	path := "/users"
+	if len(params) > 0 {
+		path = fmt.Sprintf("/users?%s", params.Encode())
+	}
+
+	response, err := c.get(path)
+	if err != nil {
+		return QueryUsersResponse{}, err
+	}
+
+	var data QueryUsersResponse
+	err = json.Unmarshal(response, &data)
+	if err != nil {
+		return QueryUsersResponse{}, err
+	}
+
+	return data, nil
 }
